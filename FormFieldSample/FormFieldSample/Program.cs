55d5564c99419db0468878b383a70948637196c7
Sameerkhan <sameerkhan.n@syncfusion.com>
2024-10-21 11:33:46 +0530

    915138_flattenform Resolved build failures.

--- FormFieldSample/FormFieldSample/Program.cs
+++ FormFieldSample/FormFieldSample/Program.cs
@@ -7,7 +7,6 @@ using Syncfusion.Pdf.Parsing;
 namespace FormFieldSample {
     internal class Program {
         static void Main(string[] args) {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2VFhhQlVEfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5Qd0FjUH5fdX1RR2ZZ  ");
 
             CreateForm();
             FillForm();
